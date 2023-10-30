f91902f3f94236e3f6b8171b94ca7c4cf420a83c
itsmesalim <itsme.salimullah@gmail.com>
2023-10-30 22:18:57 +0500

    second time commit only for ClassForSecondCommit Class

---
+++ src/JavaPractice/ClassForSecondCommit.java
@@ -0,0 +1,8 @@
+package JavaPractice;
+
+public class ClassForSecondCommit {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a class");
+        System.out.println("we write for second time commit");
+    }
+}
