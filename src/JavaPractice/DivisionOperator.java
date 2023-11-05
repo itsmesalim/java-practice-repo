eacda617e73f4b0982524b052b4fb885edc29d1e
itsmesalim <itsme.salimullah@gmail.com>
2023-11-05 09:43:36 +0500

    commit for DivisionOperator

---
+++ src/JavaPractice/DivisionOperator.java
@@ -0,0 +1,108 @@
+package JavaPractice;
+
+public class DivisionOperator {
+    public static void main(String[] args) {
+
+        System.out.println("\n dividing a int by a int gives a int \n");
+
+        int x1 = 1/2;
+        System.out.println(x1);
+
+        int x2 = 56/9;
+        System.out.println(x2);
+
+        double x3 = 1/2;
+        System.out.println(x3);
+
+        System.out.println("\n dividing a double by a double gives a double \n");
+
+        double y1 = 8.4 / 2.0;
+        System.out.println(y1);
+
+        double y2 = 63 / 4;
+        System.out.println(y2);
+
+        double y3 = 18.4f / 3.2f;
+        System.out.println(y3);
+
+        System.out.println("\n dividing a float by a float gives a float \n");
+
+        float z1 = 56.8f / 8.2f;
+        System.out.println(z1);
+
+        float z2 = 67 / 9;
+        System.out.println(z2);
+
+        float z3 = 45.4f / 6;
+        System.out.println(z3);
+
+        System.out.println("\n dividing integer and double \n");
+
+        double w1 = 67.6 / 3;
+        System.out.println(w1);
+
+        double w2 = 918 / 34.6;
+        System.out.println(w2);
+
+        System.out.println("\n dividing float and double \n");
+
+        double v1 = 456f / 45;
+        System.out.println(v1);
+
+        double v2 = 345688 / 34f;
+        System.out.println(v2);
+
+        double v3 = 567f / 89f;
+        System.out.println(v3);
+
+        double v4 = 877465957d / 79876d;
+        System.out.println(v4);
+
+
+        System.out.println("\n Different Example \n");
+
+        int i = 1/2;
+        System.out.println("i = "+i);
+
+        int j = 4/3;
+        System.out.println("j = "+j);
+
+        int m = (int) 4.6/2;
+        System.out.println("m = "+m);
+
+        int n = (int) 45.4f/4;
+        System.out.println("n = "+n);
+
+        int o = 45/(int) 3.4f;
+        System.out.println("o = "+o);
+
+        int p = (int) 567894d/45;
+        System.out.println("p = "+p);
+
+        int q = 56874/(int) 56d;
+        System.out.println("q = "+q);
+
+        int r = (int) 9488756399387l/45678;
+        System.out.println("r = "+r);
+
+        int s = (int) 88377465l/ (int) 76585.87f;
+        System.out.println("s = "+s);
+
+        float k = 4.5f/2.0f;
+        System.out.println("k = "+k);
+
+        float l = 5.3f/2;
+        System.out.println("l = "+l);
+
+        float a = (float) 758764647d/7856.43f;
+        System.out.println("a = "+a);
+
+        float b = (float) 8376464655d/5647/6754.56f;
+        System.out.println("b = "+b);
+
+        float c = 847576/847.5f+(float) 746884746d-783;
+        System.out.println("c = "+c);
+
+
+    }
+}
